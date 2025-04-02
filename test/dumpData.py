97d4a928ae52a4681ef8609dfa5a9fd9fbeea5ba
Mon <98190812+MonPanitan@users.noreply.github.com>
2025-04-02 13:04:11 +0100

    move file

--- test/dumpData.py
+++ test/dumpData.py
@@ -12,8 +12,8 @@ DATABASE_COLLECTION = "itemList"
 
 def dump_database():
     timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M")
-    backup_dir = f"database_dump{timestamp}"
-    zip_file = f"mongo_backup.zip{timestamp}"
+    backup_dir = f"itemList_backup{timestamp}"
+    zip_file = f"database_dump_{timestamp}"
 
     # Ensure backup directory exists
     if not os.path.exists(backup_dir):
@@ -22,7 +22,7 @@ def dump_database():
     try:
         # Run mongodump
         result = subprocess.run(
-            ["mongodump", "--uri", MONGO_URI, "--collection","itemList","--out", backup_dir],
+            ["mongodump", "--uri", MONGO_URI, "--collection",DATABASE_COLLECTION,"--out", backup_dir],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
@@ -32,12 +32,12 @@ def dump_database():
         print("Mongodump Error (if any):", result.stderr)
 
         # Zip the backup directory
-        shutil.make_archive("itemList_backup", "zip", backup_dir)
+        shutil.make_archive(f"database_dump_{timestamp}", "zip", backup_dir)
 
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=500, detail=f"Backup failed: {e.stderr}")
 
-    return FileResponse(zip_file, filename="database-${date}.zip", media_type="application/zip")
+    return FileResponse(zip_file, filename=zip_file, media_type="application/zip")
 
 if __name__ == "__main__":
     dump_database()
